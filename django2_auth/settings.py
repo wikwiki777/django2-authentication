cfde7521a42d30968dc2a40e57a43e5effe93a87
wicked777 <wicky.akloe@gmail.com>
2019-10-28 16:08:34 +0100

    Create base.html template

--- django2_auth/settings.py
+++ django2_auth/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'django2_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
